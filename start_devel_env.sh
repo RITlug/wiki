56c166e3c699c93c53fb4905832c1a89b6373d29
Adrian Edwards <adrian@adriancedwards.com>
2025-03-27 10:29:45 -0400

    support podman in the dev container

--- start_devel_env.sh
+++ start_devel_env.sh
@@ -4,6 +4,17 @@
 #
 
 JEKYLL_VERSION=3.8
+DOCKER_BINARY=""
+
+if command -v docker &>/dev/null; then
+    # echo "Docker is installed."
+    DOCKER_BINARY="docker"
+elif command -v podman &>/dev/null; then
+    # echo "Podman is installed."
+    DOCKER_BINARY="podman"
+else
+    echo "Neither Docker nor Podman is installed. Consider installing one of them."
+fi
 
 # Ensure _site directory is created for Jekyll engine
 if [ ! -d ./_site ]; then
@@ -11,7 +22,7 @@ if [ ! -d ./_site ]; then
 fi
 
 # Serve the site via Docker and update site when files changed in repo
-docker run --rm \
+${DOCKER_BINARY} run --rm \
     -p 4000:4000 \
     -v "$PWD:/srv/jekyll:Z" \
     -v "$PWD/.bundle-cache:/usr/local/bundle" \
